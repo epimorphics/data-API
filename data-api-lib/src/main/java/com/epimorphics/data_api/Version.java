1e85a4baa5ec51fcf6c31698e442bbb3a03bc014
Chris Dollin <ehog.hedge@googlemail.com>
2015-04-22 15:21:52 +0100

    Went to version 0.0.10A

--- data-api-lib/src/main/java/com/epimorphics/data_api/Version.java
+++ data-api-lib/src/main/java/com/epimorphics/data_api/Version.java
@@ -16,6 +16,6 @@ package com.epimorphics.data_api;
 
 */
 public interface Version {
-	public final String string = "0.0.10";
-	public final String tagname = "_(reordering)";
+	public final String string = "0.0.10A";
+	public final String tagname = "_(reordering, B)";
 }
